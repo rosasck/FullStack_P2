6accb777b9b538e3a917b15f20be2e18be84adb2
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-20 22:17:01 -0700

    made it dynamic

--- savedpets/src/index.js
+++ savedpets/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Card from './cards-together'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Card />
   </React.StrictMode>,
   document.getElementById('root')
 );
