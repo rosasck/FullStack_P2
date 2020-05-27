067a97a7e078bf03fa97b0991c786c3877541fdf
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-26 21:42:16 -0700

    adding changes

--- saved_pets_index.js
+++ saved_pets_index.js
@@ -1,7 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Card from './cards-together'
+const card = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  borderRadius: 8,
+  width: 450,
+  textAlign: 'center',
+  margin: 25,
+  backgroundColor: '#F4978E',
+}
+const heartColor = {
+  color: '#F08080',
+}
+const petPic = {
+  marginTop: 20,
+  borderRadius: 8,
+  width: 400,
+  objectFit: 'cover',
+}
+const petInfo = {
+  width: 380,
+  borderRadius: 8,
+  padding: 15,
+  margin: 15,
+  backgroundColor: '#FBC4AB',
+}
+const petData = {
+  backgroundColor:'#FBC4AB',
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ReactDOM.render(
