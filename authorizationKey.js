44aa3bb708d97e6acca8058a91146cce9d7001f3
Taylor_Atkins <43218109+TaylorAtkins@users.noreply.github.com>
2020-06-01 21:05:45 -0700

    Modified how pet-page loads info & added babble to package.json
    Modified the pet-page code to use a URL with a id parameter to load a pet on to an html page & modified package.json to include a babble module that allows us to percompile the .js files that uses babble, so we dont have to include babble in the html files. To do this, all you need to do is move the .js files into the src folder, then run this command:
    
    npx babel --watch src --out-dir . --presets react-app/prod
    
    This will compile the code & add the file outside the src file.

--- authorizationKey.js
+++ authorizationKey.js
@@ -16,3 +16,26 @@ function getToken(){
         .catch((error)=>{console.log(`ERROR MESSAGE: ${error}`); reject('failed while updating token');});
     });
 }
+
+/*
+var token;
+const clientID = 'q8AtF9GiFiVL0k38sSW1zDpA3FazOBFL1zUXxO86J7jqgN3TxW';
+const clientSecret = 'VGwQffQcjmWOpL3FBRDQiWdHBvFXkg67MFp623nm';
+const tokenUrl = 'https://api.petfinder.com/v2/oauth2/token';
+
+// Gets a new authorization token. This function should only be used if token is 
+//undefined or if a get request failed due to an expired token.
+export function getToken(){
+    return token;
+}
+export function fetchToken(){
+    return new Promise((resolve, reject)=>{
+        fetch(tokenUrl, {
+            method: 'POST',
+            headers: new Headers({'content-type': 'application/x-www-form-urlencoded'}),
+           body: `grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}`,
+        }).then((response)=>{return response.json()})
+        .then(object=>{token = object.access_token; resolve('token was updated');})
+        .catch((error)=>{console.log(`ERROR MESSAGE: ${error}`); reject('failed while updating token');});
+    });
+}*/
