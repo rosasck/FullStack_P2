bd32101156df1f0f4c68479a282822d928569a0c
eleven011 <vkillian@pdx.edu>
2020-06-04 22:31:22 -0700

    merging

--- saved_pets_index.js
+++ saved_pets_index.js
@@ -4,6 +4,7 @@
 var petArray = [];
 
 function addPet(petId) {
+    console.log("here is the pet id" + petId);
     console.log("pet added to saved pets");
     petArray.push(petId);
 }
@@ -48,7 +49,7 @@ var id = 0;
 //Function to Open up the Pet page with more information about the
 //saved Pet
 function openPetPageForPet(petID) {
-    window.location.href = './pet-page.html?id=' + petID;
+    window.location.href = "./pet-page.html?id=" + petID;
 }
 
 //Function responsible for getting the pet with the PETid from the API 
@@ -56,7 +57,7 @@ function openPetPageForPet(petID) {
 function loadPets(petId) {
 
     id = petId;
-    var petIDUrl = 'https://api.petfinder.com/v2/animals/' + id;
+    var petIDUrl = "https://api.petfinder.com/v2/animals/" + id;
     var token = localStorage.getItem('token');
 
     //this allows us to get the token validated from the API 
@@ -64,12 +65,12 @@ function loadPets(petId) {
         getToken().then(function (response) {
             token = localStorage.getItem('token');
         }).catch(function (err) {
-            console.log('ERROR MESSAGE: ' + err);
+            console.log("ERROR MESSAGE: " + err);
         });
     }
     fetch(petIDUrl, {
         headers: {
-            'Authorization': 'Bearer ' + token
+            'Authorization': "Bearer " + token
         } })
     //actual response with the pet 
     .then(function (response) {
@@ -87,16 +88,16 @@ function loadPets(petId) {
         var name = pet.name ? pet.name : "Unknown";
 
         var element = React.createElement(
-            'div',
-            { style: card, className: 'card', onClick: function onClick() {
+            "div",
+            { style: card, className: "card", onClick: function onClick() {
                     return openPetPageForPet(petId);
                 } },
-            React.createElement('img', { style: petPic, src: image, className: 'pet-pic' }),
+            React.createElement("img", { style: petPic, src: image, className: "pet-pic" }),
             React.createElement(
-                'div',
-                { style: petInfo, className: 'pet-info-more' },
+                "div",
+                { style: petInfo, className: "pet-info-more" },
                 React.createElement(
-                    'h1',
+                    "h1",
                     null,
                     name
                 )
@@ -108,7 +109,7 @@ function loadPets(petId) {
         ReactDOM.render(element, document.getElementById("rootS"));
     }).catch(function (error) {
         if (error.code) {
-            console.log('AHHHH ERROR STATUS: ' + error.code + ' ERROR MESSAGE: ' + error.message);
+            console.log("AHHHH ERROR STATUS: " + error.code + " ERROR MESSAGE: " + error.message);
         }
         //If a token is expired, gets new authorization token & re-runs the function.
         else if (error.message.includes("Failed to fetch") || error.message.includes("NetworkError")) {
@@ -135,4 +136,4 @@ function allPets() {
 //addPet(47058934);
 //addPet(48097357);
 
-allPets();
+//allPets();
