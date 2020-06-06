6eebc83a79de8675b82e7f5c376eec8188ae9e63
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-06-05 23:30:09 -0700

    db

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
+
+//this creates the pet array in local storage 
 let petArray=[]
 localStorage.setItem('savedPetsArray', JSON.stringify(petArray));
 
+
+//this is the sign in fucntion for the google API 
+//that lets me take the users ID to put in DB 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
