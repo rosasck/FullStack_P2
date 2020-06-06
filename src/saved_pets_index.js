b98c0b175101e7eda63228ae496b5712a8b9e675
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-06-05 17:20:19 -0700

    style for saved pets

--- src/saved_pets_index.js
+++ src/saved_pets_index.js
@@ -143,16 +143,24 @@ function loadPets(petId){
             )
           );
 */
-            var element= document.createTextNode("Name:  " + name);
-            var p= document.createElement('p');
-           // var el= document.createTextNode("PetPage: <a href='#' onclick='openPetPageForPet("+petId+")'> </a>");
-           p.innerHTML = `Pet Page: <a href='./pet-page.html?id=${petId}' >Click Here</a>`;
-           let h=petId;
-            //p.innerHTML = 'Pet Page: <a href="#" onclick ="openPetPageForPet(h);">Click Here</a>';
-            console.log(p);
-            
-
-             document.getElementById("rootSa").appendChild(element);
+            //var element= document.createTextNode("Name:  " + name);
+            var p= document.createElement('p' );
+           p.innerHTML = ` Pet Name: ${name} <br>  Pet Page: <a href='./pet-page.html?id=${petId}' >Click Here</a>`;
+           p.style.backgroundColor="#FBC4AB";
+           p.style.width="200px";
+           p.style.borderRadius="8px";
+           p.style.padding="15px";
+           p.style.margin="15px";
+           //p.style.cssText= "backgroundColor: '#FBC4AB';";
+            //console.log(p);
+/*
+            width: 380,
+  borderRadius: 8,
+  padding: 15,
+  margin: 15,
+  backgroundColor: '#FBC4AB', 
+*/
+             //document.getElementById("rootSa").appendChild(element);
 
             // document.getElementById("rootSa").appendChild(el);
              document.getElementById("rootSa").appendChild(p);
